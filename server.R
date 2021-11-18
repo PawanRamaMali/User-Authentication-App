dbb6578e79b4536cbe3b751685650fda019e2d02
Pawan Rama Mali <prm@outlook.in>
2021-11-18 23:54:32 +0530

    Login Functionality

--- server.R
+++ server.R
@@ -1,8 +1,8 @@
 server <- function(input, output, session) {
   
-  # * Login Functionality ----
-  # source('./srv/login.R', local = TRUE)
-  
+  #* Login Functionality ----
+  source('./srv/login.R', local = TRUE)
+
   # * Hashing Functionality ----
   source('./srv/hash.R', local = TRUE)
   
--- srv/login.R
+++ srv/login.R
@@ -1,104 +1,51 @@
 
-## Temporary Credentials ----
-cred <- read.csv("data/credentials/cred.csv")
-users <- as.data.frame(cred)
 
-## Login Set Value ----
-USER <- reactiveValues(Logged = Logged)
+user_base <- data_frame(
+  user = c("user1", "user2"),
+  password = c("pass1", "pass2"),
+  password_hash = sapply(c("pass1", "pass2"), sodium::password_store),
+  permissions = c("admin", "standard"),
+  name = c("User One", "User Two")
+)
 
 
-## Validate Login ----
-observeEvent(input$Login, {
-  
-  #* Login Error Message ----
-  output$message <- renderUI({
-    
-    if (as.character(input$userName) == "" || as.character(input$passwd) == ""){
-      HTML(
-        "<div id='error-box'>
-                Username or password cannot be blank !! 
-                 </div>"
-      )
-    }
-    
-    else if (!is.null(input$Login)) {
-      my_username <-
-        length(users$User[grep(pattern = input$userName, x = users$User)])
-      my_password <-
-        length(users$User[grep(pattern = input$passwd, x = users$Password)])
-      if (input$Login > 0) {
-        if (my_username < 1 ||  my_password < 1) {
-          HTML(
-            "<div id='error-box'>
-                 Incorrect Username or Password. Please,
-                 try again. If you continue to have problems,
-                 <a href='https://github.com/pawanramamali'>
-                 <u>Contact Us..</u></a>
-                 </div>"
-          )
-        }
-      }
-    }
-    
-  })
-  
-  # * Verify Input ----
-  if (USER$Logged == FALSE) {
-    if (!is.null(input$Login)) {
-      if (input$Login > 0) {
-        Username <- isolate(input$userName)
-        Password <- isolate(input$passwd)
-        Id.username <- which(users$User == Username)
-        Id.password <- which(users$Password == Password)
-        if (length(Id.username) > 0 & length(Id.password) > 0) {
-          if (Id.username %in% Id.password) {
-            USER$Logged <- TRUE
-          }
-        }
-      }
-    }
-  }
+credentials <- callModule(
+  login,
+  "login",
+  data = user_base,
+  user_col = user,
+  pwd_col = password_hash,
+  sodium_hashed = TRUE,
+  log_out = reactive(logout_init())
+)
+
+logout_init <-
+  callModule(logout, "logout", reactive(credentials()$user_auth))
+
+
+
+user_info <- reactive({
+  credentials()$info
 })
 
-## Display UI ----
-observe({
-  # * Logged Status False ----
-  if (USER$Logged == FALSE) {
-    output$page <- renderUI({
-      div(class = "outer",
-          do.call(bootstrapPage,
-                  c("", login_page())))
-    })
+
+
+output$home_page <- renderUI({
+  req(credentials()$user_auth)
+  
+  if (user_info()$permissions == 'admin') {
+    # Admin Page ----
+    source('ui/admin/admin_page.R', local = TRUE)$value
+    
   }
   
-  # * Logged Status True  ----
-  if (USER$Logged == TRUE)
+  else
+    
   {
-    ## Current user's authorization level check
-    uid <- which(users$User ==input$userName)
-    user_dept <- users$Dep[uid]
-    if (user_dept == "agent") {
-      output$page <- renderUI({
-        # User Page ----
-        source('ui/admin/admin_page.R', local = TRUE)$value
-      })
-    }
+    # User Page ----
+    source('ui/user/user_page.R', local = TRUE)$value
     
-    
-    else if (user_dept == "admin") {
-      output$page <- renderUI({
-        # Admin Page ----
-        source('ui/admin/admin_page.R', local = TRUE)$value
-      })
-    }
-    
-    # if user not assigned dept
-    else{
-      output$page <- renderUI({
-        div(class = "outer",
-            do.call(bootstrapPage,
-                    c("", login_page())))
-      })
-    }
   }
+  
+  
 })
