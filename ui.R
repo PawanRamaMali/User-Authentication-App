90b0e5d763991e470cb2bb84dbd699f82f4a4570
Pawan Rama Mali <prm@outlook.in>
2021-11-18 23:03:22 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,30 +1,7 @@
-# ui = (uiOutput("page"))
-
-
-fluidPage(title = "Login Page",
-          shinyjs::useShinyjs(),
-          # fluidRow(
-          #   tags$li(class = "dropdown", style = "padding: 8px;",
-          #           logoutUI("logout")),
-          #   tags$li(class = "dropdown", 
-          #           tags$a(icon("help"), 
-          #                  href = "https://github.com/",
-          #                  title = "GitHub Code"))
-          # ),
-          
-          fluidRow(
-            
-            # 
-            # tags$head(tags$style(".table{margin: 0 auto;}"),
-                      
-                      includeScript("functions/returnClick.js")
-            ),
-            loginUI("login"),
-            
-            uiOutput("home_page")
-          # ,
-          #   HTML('<div data-iframe-height></div>')
-          #   
-          #   
-          # )
+fluidPage(
+  title = "Login Page",
+  shinyjs::useShinyjs(),
+  includeScript("functions/returnClick.js"),
+  loginUI("login"),
+  uiOutput("home_page")
 )
