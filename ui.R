3728bfd1dcec5878d32d955cb611ea2416fb2369
Pawan Rama Mali <prm@outlook.in>
2021-11-25 22:23:11 +0530

    v2.0

--- ui.R
+++ ui.R
@@ -1,7 +1,11 @@
-fluidPage(
-  title = "Login Page",
-  shinyjs::useShinyjs(),
-  includeScript("functions/returnClick.js"),
-  loginUI("login"),
-  uiOutput("home_page")
-)
+ui <- 
+  fluidPage(title = "Login Page",
+          
+            shinyjs::useShinyjs(),
+            fluidRow(
+              tags$div(class = "logout-action-button",
+                       logoutUI("logout"))),
+            includeScript("functions/returnClick.js"),
+            loginUI("login", title = "Login",),
+            uiOutput("home_page")
+  )
